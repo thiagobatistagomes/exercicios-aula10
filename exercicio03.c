652f2fcff9f6cc788ff098e07ed08ab48bc399d5
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2023-06-20 16:07:11 -0300

    ex03 funcional

--- exercicio03.c
+++ exercicio03.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 /*
     Crie uma estrutura representando os dados de um aluno: matrícula, nome, nota da primeira prova, nota da segunda prova e nota da terceira prova. A seguir: a) entre com os dados de 5 alunos. b) encontre o aluno com maior nota na primeira prova, imprimindo seu nome completo e a nota, com 2 casas decimais. c) encontre o aluno com maior média geral, imprimindo seu nome completo, seguido da média, com 2 casas decimais. d) imprima uma listagem de matrícula e nome, dizendo também se cada aluno foi aprovado ou reprovado, considerando ao menos 60% de média para a aprovação.
@@ -19,7 +18,6 @@ int main ()
     aluno alunos[TAM];
     int i, cont = 0;
     float maiorn1, maiormedia;
-    char nome[30];
 
     //preenchendo os dados dos alunos
 
@@ -98,6 +96,27 @@ int main ()
         {
             printf("Nome: %s\n", alunos[i].nome);
             printf("Media: %.2f\n", maiormedia);
+        }
+    }
+
+    printf("\n");
+
+    //imprimir uma listagem de matrícula e nome, dizendo também se cada aluno foi aprovado ou
+    // reprovado considerando ao menos 60% de média para a aprovação.
+
+    for(i = 0; i < TAM; i++)
+    {
+        if((alunos[i].n1 + alunos[i].n2 + alunos[i].n3)/ 3 > 6)
+        {
+            printf("Nome: %s\n", alunos[i].nome);
+            printf("Nota: %.2f\n", (alunos[i].n1 + alunos[i].n2 + alunos[i].n3)/ 3);
+            printf("Aprovado!\n");
+        }
+        else
+        {
+            printf("Nome: %s\n", alunos[i].nome);
+            printf("Nota: %.2f\n", (alunos[i].n1 + alunos[i].n2 + alunos[i].n3)/ 3);
+            printf("Reprovado!\n");
         }
     }
 
