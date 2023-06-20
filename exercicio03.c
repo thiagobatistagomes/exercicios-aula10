d6a63ae23840344c6b5029070854dc1906594d84
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2023-06-20 15:46:16 -0300

    fix ex03

--- exercicio03.c
+++ exercicio03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
     Crie uma estrutura representando os dados de um aluno: matrícula, nome, nota da primeira prova, nota da segunda prova e nota da terceira prova. A seguir: a) entre com os dados de 5 alunos. b) encontre o aluno com maior nota na primeira prova, imprimindo seu nome completo e a nota, com 2 casas decimais. c) encontre o aluno com maior média geral, imprimindo seu nome completo, seguido da média, com 2 casas decimais. d) imprima uma listagem de matrícula e nome, dizendo também se cada aluno foi aprovado ou reprovado, considerando ao menos 60% de média para a aprovação.
@@ -16,8 +17,9 @@ typedef struct aluno
 int main ()
 {
     aluno alunos[TAM];
-    int i;
+    int i, cont = 0;
     float maiorn1, maiormedia;
+    char nome[30];
 
     //preenchendo os dados dos alunos
 
@@ -42,19 +44,39 @@ int main ()
         {
             maiorn1 = alunos[i].n1;
         }
-    }
-
-    for(i = 0; i < TAM; i++)
-    {
-        if(alunos[i].n1 == maiorn1)
+        else if(alunos[i].n1 == maiorn1) // contando quantas vezes a maior nota aparece
         {
-            printf("Aluno com maior nota na primeira prova:\n");
-            printf("Nome: %s\n", alunos[i].nome);
-            printf("Nota: %.2f\n", alunos[i].n1);
-            printf("\n");
-          
+            cont++;
         }
     }
+
+    if(cont == 1) // se a maior n1 aparecer apenas uma vez
+    {
+        printf("O Aluno com maior Nota de N1 eh:\n");
+        for(i = 0; i < TAM; i++)
+        {
+            if(alunos[i].n1 == maiorn1)
+            {
+                printf("Nome: %s\n", alunos[i].nome);
+            }
+        }
+        printf("Nota: %.2f\n", maiorn1);
+    }
+    else // se aparecer mais de uma vez
+    {
+        printf("Alunos com maior Nota de N1:\n");
+        for(i = 0; i < TAM; i++)
+        {
+            if(alunos[i].n1 == maiorn1)
+            {
+                printf("Nome: %s\n", alunos[i].nome);
+            }
+        }
+        printf("Nota: %.2f\n", maiorn1);
+    }
+
+    printf("\n");
+    
 
     //encontrar o aluno com maior media geral, imprimir seu nome completo, seguido da média, 2 casas decimais.
 
@@ -68,12 +90,14 @@ int main ()
         }
     }
 
+    printf("Aluno(os) com maior media geral:\n");
+
     for(i = 0; i < TAM; i++)
     {
         if((alunos[i].n1 + alunos[i].n2 + alunos[i].n3)/ 3 == maiormedia)
         {
             printf("Nome: %s\n", alunos[i].nome);
-            printf("Media: %.2f", maiormedia);
+            printf("Media: %.2f\n", maiormedia);
         }
     }
 
